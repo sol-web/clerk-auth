56ed1f04f6a33b083dcee695d31ccd7248b87918
sol-web <sesi3home@gmail.com>
2023-09-26 22:17:22 +0900

    ...

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -9,16 +9,6 @@ const DashboardPage = async () => {
     <div>
       <h1 className="text-2xl fonst-bold mb-5 mt-20 ">Dashboard</h1>
       <p className="mb-5">Welcom to Dashboard</p>
-      <table border={5} width="800px">
-        <tr>
-          <th rowSpan={2}>UserID:</th>
-          <th rowSpan={2}>{userId}</th>
-        </tr>
-        <tr>
-          <td>username:</td>
-          <td rowSpan={2}> {user?.username}</td>
-        </tr>
-      </table>
     </div>
   )
 }
